b8f6b09244e987f6edfc88dd5a9f19b6fcdb83d0
MDMairet <mdmairet@users.noreply.github.com>
2019-08-15 16:51:38 -0700

    Add files via upload

--- Notebooks/python/NotebookStudent1.py
+++ Notebooks/python/NotebookStudent1.py
@@ -1,50 +1,31 @@
 # ---
 # jupyter:
-#   celltoolbar: Create Assignment
 #   jupytext:
-#     cell_metadata_filter: all
 #     formats: ipynb,py:percent
-#     notebook_metadata_filter: all,-language_info
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.1
+#       jupytext_version: 1.2.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
+
 # %% [markdown]
-# Demo Manually graded answer
+# Excercis 1
 #
-# Please state Newton's three law of motion:
-# %% {"nbgrader": {"grade": true, "grade_id": "newton_law_motion", "locked": false, "points": 1, "schema_version": 3, "solution": true, "task": false}}
-# %% [markdown]
-# Demo Autograded answer
-#
-# Please write the Linear Momentum formula in the below cell:
-# %% [markdown] {"nbgrader": {"grade": false, "grade_id": "momentum_formula", "locked": false, "schema_version": 3, "solution": true, "task": false}}
+# a) Create a variable t with a value of 5
+
+# %% {"nbgrader": {"grade": false, "grade_id": "variable", "locked": false, "schema_version": 3, "solution": true, "task": false}}
 ### BEGIN SOLUTION
-# p = mv
+t = 5
 ### END SOLUTION
+
 # %% [markdown]
-# Demo Autograder tests
+# b) The below code should run without any error
+
 # %% {"nbgrader": {"grade": true, "grade_id": "cell-d4a12ebfd43a43ba", "locked": true, "points": 1, "schema_version": 3, "solution": false, "task": false}}
-### BEGIN HIDDEN TESTS
-a = 5
-assert a == 5
-### END HIDDEN TESTS
-
-# %% [markdown]
-# Demo Read-only
-
-# %% {"nbgrader": {"grade": false, "grade_id": "cell-0ab772a9f6f882ef", "locked": true, "schema_version": 3, "solution": false, "task": false}}
-a = 1 + 1
-assert a == 2
-
-# %% [markdown]
-# Manually graded task
-
-# %% [markdown] {"nbgrader": {"grade": true, "grade_id": "cell-f007c5d5b4e88524", "locked": false, "points": 1, "schema_version": 1, "solution": true, "task": true}, "trusted": true}
-# Describe the task here!
+s = 10/t
+print (s)
