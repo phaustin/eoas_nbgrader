672925ad2845465e684cda5a09f5e5721030f739
MDMairet <mdmairet@users.noreply.github.com>
2019-08-16 13:26:54 -0700

    Add files via upload

--- Notebooks/python/NotebookStudent3.py
+++ Notebooks/python/NotebookStudent3.py
@@ -1,28 +1,55 @@
 # ---
 # jupyter:
-#   celltoolbar: Create Assignment
 #   jupytext:
-#     cell_metadata_filter: all
 #     formats: ipynb,py:percent
-#     notebook_metadata_filter: all,-language_info
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.1
+#       jupytext_version: 1.2.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
-# %% [markdown] {"trusted": true}
-# Question 1
-# %% {"nbgrader": {"schema_version": 3, "solution": false, "grade": true, "locked": true, "task": false, "points": 1, "grade_id": "cell-e96f567728b7bd7d"}}
-# %% [markdown] {"trusted": true}
-# Question 2
-# %% [markdown] {"nbgrader": {"schema_version": 3, "solution": true, "grade": false, "locked": false, "task": false, "grade_id": "cell-f409ae4c0190c596"}, "trusted": true}
+
+# %% [markdown]
+# Excercise Playing with pyplot
 #
-# %% [markdown] {"trusted": true}
-# Question 3
-# %% [markdown] {"nbgrader": {"schema_version": 3, "solution": false, "grade": false, "locked": true, "task": true, "points": 1, "grade_id": "cell-fe5be254a7580a61"}, "trusted": true}
-# Describe the task here!
+# a) In the below code, insert a statement that will generate a plot from the following data:
+#
+# [1,2,3,4],[9,4,9,16]
+#
+# Go here (https://matplotlib.org/) to read more about Matplotlib.
+#
+# After inserting the statement, run the code to display the plot.
+#
+
+# %% {"nbgrader": {"schema_version": 3, "solution": true, "grade": true, "locked": false, "task": false, "points": 1, "grade_id": "cell-e96f567728b7bd7d"}}
+import matplotlib.pyplot as plt
+##insert your statement here##
+plt.xlabel ('X')
+plt.ylabel('Y')
+plt.show()
+
+
+# %% [markdown]
+# b) Run the below code.  Nothing should be displayed.
+
+# %% {"nbgrader": {"schema_version": 3, "solution": true, "grade": false, "locked": false, "task": false, "grade_id": "cell-f409ae4c0190c596"}}
+import numpy as np  
+import matplotlib.pyplot as plt  
+def graph(formula, x_range):  
+    x = np.array(x_range)  
+    y = eval(formula)
+    plt.plot(x, y)  
+    plt.show()
+
+# %% [markdown]
+# c) Now run the following statement: graph('X', range(-10, 11)) in the cell below replacing x with any formula that you like.  For example, replace x with 'x**3+2*x-3'.
+#
+# This statement call the above script and insert a formula to be plotted.
+
+# %% {"nbgrader": {"schema_version": 3, "solution": false, "grade": false, "locked": true, "task": true, "points": 1, "grade_id": "cell-fe5be254a7580a61"}}
+
+# %%
