9d21d5eebb6a2888406b9253c2fb5c7b831f3ed7
MDMairet <mdmairet@users.noreply.github.com>
2019-08-19 13:16:28 -0700

    Add files via upload

--- Notebooks/python/NotebookStudent5.py
+++ Notebooks/python/NotebookStudent5.py
@@ -1,27 +1,35 @@
 # ---
 # jupyter:
-#   celltoolbar: Create Assignment
 #   jupytext:
-#     cell_metadata_filter: all
 #     formats: ipynb,py:percent
-#     notebook_metadata_filter: all,-language_info
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.1
+#       jupytext_version: 1.2.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
-# %% [markdown] {"trusted": true}
+
+# %% [markdown]
 # Question 1
-# %% {"nbgrader": {"schema_version": 3, "solution": false, "grade": true, "locked": true, "task": false, "points": 1, "grade_id": "cell-a42a7f03915765f5"}}
-# %% [markdown] {"trusted": true}
-# Question 2
-# %% [markdown] {"nbgrader": {"schema_version": 3, "solution": false, "grade": false, "locked": true, "task": true, "points": 1, "grade_id": "cell-00cc95148a616c15"}, "trusted": true}
-# Describe the task here!
-# %% [markdown] {"trusted": true}
-# Question 3
-# %% {"nbgrader": {"schema_version": 3, "solution": true, "grade": true, "locked": false, "task": false, "points": 1, "grade_id": "cell-7884b157e9414455"}}
+#
+# a) Numpy array can contain words.  You can loop over the elements of a list. Run the code below to display a list of planets.
+
+# %% {"nbgrader": {"schema_version": 3, "solution": true, "grade": true, "locked": false, "task": false, "points": 1, "grade_id": "cell-a42a7f03915765f5"}}
+import numpy as np
+
+planets = np.array(['Jupiter', 'Earth', 'Mars', 'Venus', 'Pluto'])
+
+for planet in planets:
+    print (planet)
+
+# %% [markdown]
+# b) Write a statement in the cell below that will delete the element that does not belong in this list (hint: no a planet).
+
+# %% {"nbgrader": {"schema_version": 3, "solution": true, "grade": false, "locked": false, "task": false, "grade_id": "cell-00cc95148a616c15"}}
+### BEGIN SOLUTION
+planets = np.delete(planets, 5)
+### END SOLUTION
